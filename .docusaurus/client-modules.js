91355264c9236175e07ac1686cddd7d019eeca32
Aqsa <161750279+Aqsa-owais@users.noreply.github.com>
2025-12-16 16:46:54 +0500

    first commit

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("D:\\giaic-hackathon-speckit-plus\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("D:\\giaic-hackathon-speckit-plus\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("D:\\giaic-hackathon-speckit-plus\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("D:\\giaic-hackathon-speckit-plus\\src\\css\\custom.css"),
-];
